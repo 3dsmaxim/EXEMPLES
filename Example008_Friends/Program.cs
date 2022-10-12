c9b08ffbe3c06081b64bbf05fdc538f8ede3d45f
Maxim Stepanyuk <3dsmaxim@mail.ru>
2022-10-13 00:22:25 +0300

    add example008 friends

---
+++ Example008_Friends/Program.cs
@@ -0,0 +1,32 @@
+﻿int count =0;
+int time = 0;
+
+int distance = 10000;
+
+int FirstFriandSpeed = 1;
+
+int SecondFriandSpeed = 2;
+
+int DogSpeed = 5;
+
+int Friand = 2;
+
+
+while (distance > 10)
+{
+    if (Friand == 1)
+    {
+        time = distance / (FirstFriandSpeed + DogSpeed);
+        Friand = 2;
+    }
+    else 
+    {
+        time = distance / (SecondFriandSpeed + DogSpeed);
+        Friand = 1;
+    }
+    distance = distance - (FirstFriandSpeed + SecondFriandSpeed) * time;
+    count++;
+}
+Console.Write("Шарик сбегад туда сюда ");
+Console.Write(count);
+Console.WriteLine(" раз.");
