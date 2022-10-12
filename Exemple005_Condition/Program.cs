53a6145e533de161e331efb1ac969b673929fd0a
Maxim Stepanyuk <3dsmaxim@mail.ru>
2022-10-12 19:04:01 +0300

    Example 005 Condition

---
+++ Exemple005_Condition/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Уа, это же МАША!");
+}
+else
+{
+    Console.Write("Нахер иди, ");
+    Console.Write(username);
+    Console.WriteLine(", где МАША!!!");
+}
