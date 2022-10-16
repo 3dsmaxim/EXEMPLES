f4809f37aaa66dd6fc275989d9aa04926e8e09be
Maxim Stepanyuk <3dsmaxim@mail.ru>
2022-10-16 16:03:17 +0300

    add in example011 добавили методы

--- Example011_IntroArray/Program.cs
+++ Example011_IntroArray/Program.cs
@@ -47,3 +47,33 @@
 
 // Вариант 3
 
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+
+        index++;
+    }
+}
+
+void PrintArrey(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position] + ", ");
+        position++;
+    }
+
+}
+
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArrey(array);
