1644e19183053cb272d856c17a43202a1ba98486
Maxim Stepanyuk <3dsmaxim@mail.ru>
2022-11-19 23:41:48 +0300

    лекция 5

---
+++ Example014_no1_no/Program.cs
@@ -0,0 +1,15 @@
+﻿// Console.Clear();
+
+
+// using System.Linq;
+// var a = 12;
+// // Console.WriteLine(a.GetType());
+
+
+// var data = new int[] { 1, 2, 3, 4 }
+//            .Where(e => e > 0)
+//            .Select(e => new { q = e, W = e + 1 });
+// Console.WriteLine(data.GetType().Name);
+
+
+
