602dc9f78d48de0bbe08fbe74fb62511d3b91612
Maxim Stepanyuk <3dsmaxim@mail.ru>
2022-11-19 14:41:54 +0300

    лукция 4

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -100,33 +100,33 @@
 
 
 
-// string text = "— Я думаю, — сказал князь, улыбаясь, — что,"
-//             + "ежели бы вас послали вместо нашего милого Винценгероде,"
-//             + "вы бы взяли приступом согласие прусского короля."
-//             + "Вы так красноречивы. Вы дадите мне чаю?";
+string text = "— Я думаю, — сказал князь, улыбаясь, — что,"
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля."
+            + "Вы так красноречивы. Вы дадите мне чаю?";
 
-// string Replace(string text, char oldV, char newV)
-// {
-//     string result = String.Empty;
-//     int length = text.Length;
-//     for (int i = 0; i < length; i++)
-//     {
-//         if (text[i] == oldV) result = result + $"{newV}";
-//         else result = result + $"{text[i]}";
-//     }
-//     return result;
-// }
-// string newText = Replace(text, ' ', '|');
-// Console.WriteLine(newText);
-// Console.WriteLine();
+string Replace(string text, char oldV, char newV)
+{
+    string result = String.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldV) result = result + $"{newV}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
 
-// newText = Replace(newText, 'к', 'К');
-// Console.WriteLine(newText);
-// Console.WriteLine();
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
 
-// newText = Replace(newText, 'с', 'С');
-// Console.WriteLine(newText);
-// Console.WriteLine();
+newText = Replace(newText, 'с', 'С');
+Console.WriteLine(newText);
+Console.WriteLine();
 
 
 
