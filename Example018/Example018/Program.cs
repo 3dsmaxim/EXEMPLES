4f0aabf8e53707a8947a56a70d137f0ba20c1685
Maxim Stepanyuk <3dsmaxim@mail.ru>
2022-11-21 19:27:00 +0300

    Лекция 8

---
+++ Example018/Example018/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using ListMaster;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+
+
+namespace ConsoleVS
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            ArrayMaster am = new();
+            for (int i = 0; i < 10; i++)
+            {
+                am.Add(new Random().Next(1, 5));
+            }
+            for (int i = 0; i < am.GetCount(); i++)
+            {
+                Console.WriteLine(am.GetValue(i));    
+            }
+        }
+
+     
+
+   }
+}
